50b40254ba3aabe32290c0cf4e43411777255d78
tfaulkner80 <63273613+tfaulkner80@users.noreply.github.com>
2020-04-08 22:58:31 -0400

    Add files via upload
    first submission

---
+++ P3HW1_ColorMix_TerranceFaulkner.py
@@ -0,0 +1,15 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> CTI-110
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    CTI-110
+NameError: name 'CTI' is not defined
+>>> #CTI-110
+>>> #P3HW1 - Color Mixer
+>>> #Terrance Faulkner
+>>> #4/8/20
+>>> #Color ('red') color ('blue')
+>>> #color ('red') color ('yellow')
+>>> #color ('blue') color ('yellow')
+>>> 
